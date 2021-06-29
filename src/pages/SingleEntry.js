7e42f32b54cd1256bac9988a9e440ce6cd0f8788
fawzian <fawzianur@gmail.com>
2021-06-28 20:08:27 -0400

    creating basics of the show page

--- src/pages/SingleEntry.js
+++ src/pages/SingleEntry.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Entry = (props) => {
-    return <h1>Entry</h1>
+const SingleEntry = (Entry, match) => {
+    const id = parseInt(match.params.id);
+    const entry = entry.find((entry) => entry.id === id);
+
+    // styles
+    const div ={
+        textAlign: "center",
+        border: "3px solid green",
+        width: "80%",
+        margin: "30px auto",
+    };
+    return (
+        <div style={div}>
+            <h1>{entry.title}</h1>
+            <h2>{entry.details}</h2>
+            <Link to="/">
+                <button>Go Back</button>
+            </Link>
+        </div>
+    );
 };
 
-export default Entry;
+export default SingleEntry;
